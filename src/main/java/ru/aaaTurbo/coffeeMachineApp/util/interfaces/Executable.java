428894f48a7dd753684e7e75259894a8e9ea57da
Anton Semenov <antsemion@gmail.com>
2023-11-15 14:18:13 +0300

    Create Executable.java
    Abstraction for commands. Describes base functional.

---
+++ src/main/java/ru/aaaTurbo/coffeeMachineApp/util/interfaces/Executable.java
@@ -0,0 +1,7 @@
+package ru.aaaTurbo.coffeeMachineApp.util.interfaces;
+
+import ru.aaaTurbo.coffeeMachineApp.util.abstracts.CoffeeMachine;
+
+public interface Executable {
+    String execute(CoffeeMachine coffeeMachine);
+}
